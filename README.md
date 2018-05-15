2dbeefa6808584f62eb04777e3fd53216dfa74e9
Bitcoin Incognito <39087531+BTCIncognito@users.noreply.github.com>
2018-05-14 22:05:05 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# btcimn
+# BTCi Masternode Install Script
+# Use this script on a fresh install of Ubuntu 14.04 - MUST BE 14.04 x64
+# Use this script on a fresh install of Ubuntu 14.04 - MUST BE 14.04 x64
+
+Stable and cheap host here https://m.do.co/c/b93026fab810
+
+BTCi address if this helped you and you love me: B654n7tNRTUhdE5ojSiWK5v5pvbR19G9j8
+
+# This guide is meant for setting up a HOT/COLD SETUP WITH WINDOWS AND LINUX.
+
+# Use this script on a fresh install of Ubuntu 14.04 - MUST BE 14.04 x64
+# Use this script on a fresh install of Ubuntu 14.04 - MUST BE 14.04 x64
+
+# Part 1 - Sending Collateral Coins
+
+1. Open your Windows wallet - MAKE SURE IT IS SYNCED WITH THE NETWORK
+2. Go to Tools -> Debug Console
+3. Type: getaccountaddress MN# (# is your masternode number you want to use)
+4. Send 1500 BTCi to this address
+5. Go to Tools -> Debug Console
+6. Type: masternode outputs (This can take a minute before an output is shown)
+7. Type: masternode genkey
+7. Save your TX ID (The first number) and your Index Number (Second number, either a 1 or 0)
+8. Save your generated key as well as this will be needed in your VPS as your private key
+9. Save these with a notepad
+10. Close the wallet
+11. Move to Part 2 for now
+
+# Part 2 - Getting your Linux VPS Started Up (Read all instructions and follow prompts closely)
+
+1. Connect to your linux vps AS ROOT (AWS USERS USE sudo -i TO LOGIN AS ROOT), copy and paste the following to get started:
+```
+cd && sudo apt-get -y install git && sudo git clone https://github.com/testiface/test.git && cd test/ && sudo bash install.sh
+```
+2. follow the prompts closely and don't mess it up!
+3. Move to Part 3
+
+# Part 3 - Editing your Windows Config File
+
+1. Open your wallet
+2. Go to Tools -> Open Masternode Configuration File
+3. Enter the following on one single line after the example configuration
+```<alias> <ip>:7250 <private_key> <tx_id> <index>```
+4. It should look something like this:
+``` MN1 66.65.43.32:7250 87dfjnKNfdjNlwomdmKKMdkaNIE a3eofJJkdlMlfKokfmalmofO 0```
+5. Save and close the file and restart your wallet.
+
+# Part 4 - Starting the Masternode
+
+1. In your wallet, go to Tools -> Debug Console
+2. Enter ```startmasternode alias 0 <alias>``` with ```<alias>``` being the name of your masternode from Part 3
+3. Enjoy!  You can start this process over again for another MN on a fresh Linux VPS!
+
+# Bronate if this helped
+
+*Official BTCi Discord: https://discord.gg/4YVntwG
+
+
+# Recommended Tools
+
+- Putty - Easy to use and customizable SSH client.
+- SuperPutty - This allows you to have multiple Putty tabs open in one window, allowing you to easily organize and switch between masternode servers.
+
---
+++ install.sh
@@ -0,0 +1,90 @@
+#/bin/bash
+
+echo "*******************************"
+echo "*                             *"
+echo "*      BTCi Masternode        *"
+echo "*           SETUP             *"
+echo "*            BY               *"
+echo "*         CryptoBat           *"
+echo "*                             *"
+echo "*                             *"
+echo "*******************************"
+echo ""
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!                                                 !"
+echo "! Make sure you double check before hitting enter !"
+echo "!                                                 !"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo && echo && echo
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+IP==$(curl -s4 icanhazip.com)
+echo -e "${GREEN}Please enter your private key: (Right click and press enter)${NC}" 
+read KEY
+sleep 2
+echo ""
+echo ""
+echo -e "${RED}PLEASE KEEP YOUR HANDS AND FEET INSIDE THE RIDE AT ALL TIMES!${NC}"
+sleep 5
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install wget nano unrar unzip -y
+sudo apt-get install libboost-all-dev libevent-dev software-properties-common -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sleep 2
+echo -e "${GREEN}Downloading more ram!${NC}"
+sleep 5 
+cd /var
+sudo touch swap.img
+sudo chmod 600 swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo free
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+cd
+sleep 2
+echo -e "${GREEN}Keeping out all the ${RED}HAXORZ!${NC}"
+sleep 5
+sudo apt-get install -y ufw
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw allow 7250/tcp
+sudo ufw logging on
+echo "y" | sudo ufw enable
+sleep 2
+echo ""
+echo ""
+echo -e "${GREEN}Moving stuff where it ${RED}NEEDS to be${NC}"
+sudo chmod +x /root/test/btcid /root/test/btci-cli
+sudo mv /root/test/btcid /root/test/btci-cli /usr/local/bin
+echo ""
+echo ""
+sleep 2
+echo -e "${GREEN}Creating the ${RED}Configuration File Now${NC}"
+sleep 2
+sudo mkdir /root/.BTCi
+echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /root/.BTCi/btci.conf
+echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /root/.BTCi/btci.conf
+echo "rpcallowip=127.0.0.1" >> /root/.BTCi/btci.conf
+echo "listen=1" >> /root/.BTCi/btci.conf
+echo "server=1" >> /root/.BTCi/btci.conf
+echo "daemon=1" >> /root/.BTCi/btci.conf
+echo "maxconnections=250" >> /root/.BTCi/btci.conf
+echo "masternode=1" >> /root/.BTCi/btci.conf
+echo "masternodeaddr$IP:7250" >> /root/.BTCi/btci.conf
+echo "externalip$IP:7250" >> /root/.BTCi/btci.conf
+echo "masternodeprivkey=$KEY" >> /root/.BTCi/btci.conf
+echo -e "${GREEN}STARTING THE DAEMON${NC}"
+cd /root
+sudo btcid
+sleep 2
+echo ""
+echo ""
+echo -e "Thank you for installing BTCi.  Please move onto the ${RED}NEXT${NC} step."
